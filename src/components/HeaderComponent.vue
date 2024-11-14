8ec741548adc26689496205fce403034157d75f5
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-11-14 12:40:01 +0100

    JS added for the toggle

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,30 @@
 <script>
 export default {
   name: "HeaderComponent",
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    // To make the list appear when the hamburger menu is clicked
+    toggleNavbarHamburger() {
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav-menu");
+
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    },
+
+    // To make the list disappear when a link is clicked
+    listDisappearance() {
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav-menu");
+
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    },
+  },
 };
 </script>
 
@@ -21,35 +45,35 @@ export default {
         <!-- END Logo -->
 
         <div class="col-70 d-flex">
-          <nav class="navbar">
+          <nav class="navbar d-flex">
+            <ul class="nav-menu d-flex">
+              <li class="nav-item">
+                <a @click="listDisappearance()" class="nav-link">
+                  <router-link :to="{ name: 'Insubria' }" class="link">
+                    Insubria
+                  </router-link>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a @click="listDisappearance()" class="nav-link">
+                  <router-link :to="{ name: 'Team' }" class="link"
+                    >Team</router-link
+                  >
+                </a>
+              </li>
+              <li class="nav-item">
+                <a @click="listDisappearance()" class="nav-link">esfewf</a>
+              </li>
+            </ul>
+            <div @click="toggleNavbarHamburger()" class="hamburger">
+              <span class="bar"></span>
+              <span class="bar"></span>
+              <span class="bar"></span>
+            </div>
             <div>
-              <ul class="nav-menu d-flex">
-                <li class="nav-item">
-                  <a class="nav-link">
-                    <router-link :to="{ name: 'Insubria' }">
-                      Cos'è Insubria
-                    </router-link>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link">
-                    <router-link :to="{ name: 'Team' }">Team</router-link>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link">esfewf</a>
-                </li>
-              </ul>
-              <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-              </div>
+              <button>Language</button>
             </div>
           </nav>
-          <div>
-            <button>Language</button>
-          </div>
         </div>
       </div>
     </div>
@@ -57,18 +81,14 @@ export default {
 </template>
 
 <style scoped lang="scss">
-.container {
-  background-color: red;
-}
-
 .col-30 {
   width: 30%;
+  text-align: center;
   background-color: yellow;
 }
 
 .logo img {
-  width: 70%;
-  padding-left: 10px;
+  width: 45%;
   padding-top: 10px;
   padding-bottom: 10px;
 }
@@ -78,8 +98,72 @@ export default {
   background-color: aqua;
 }
 
-.nav-menu {
+.navbar {
   align-items: center;
-  gap: 60px;
+
+  .nav-item {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  .link {
+    text-decoration: none;
+  }
+}
+
+.nav-link {
+  transition: 0.7s ease;
+}
+
+.hamburger {
+  display: none;
+  cursor: pointer;
+}
+
+.bar {
+  display: block;
+  width: 25px;
+  height: 3px;
+  margin: 5px auto;
+  background-color: black;
+  -webkit-transition: all 0.7s ease-in-out;
+}
+
+// Media queries
+@media (max-width: 768px) {
+  .navbar {
+    position: relative;
+  }
+
+  .nav-menu {
+    position: fixed;
+    left: -100%;
+    top: 146px;
+    flex-direction: column;
+    width: 100%;
+    text-align: center;
+    transition: 1.3s;
+    background-color: red;
+  }
+
+  .hamburger {
+    display: block;
+  }
+
+  .hamburger.active .bar:nth-child(2) {
+    opacity: 0;
+  }
+
+  .hamburger.active .bar:nth-child(1) {
+    transform: translateY(8px) rotate(45deg);
+  }
+
+  .hamburger.active .bar:nth-child(3) {
+    transform: translateY(-8px) rotate(-45deg);
+  }
+
+  .nav-menu.active {
+    left: 0;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
+
+// Importing Vue Router
 import { router } from "./route";
+
+// Importing the SCSS styles file
 import "./style.scss";
+
+// Importing Vue root component
 import App from "./App.vue";
 
 createApp(App).use(router).mount("#app");
