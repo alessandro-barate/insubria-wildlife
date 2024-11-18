32f55ec8818105590e53fdc8ca1a877a9e81987f
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-11-19 00:30:06 +0100

    Fontawesome installed

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -7,12 +7,27 @@ export default {
 <template>
   <footer>
     <div class="container">
-      <div class="row d-flex">
-        <div class="col">
-          <div class="col-60">
-            <h2>Ciao</h2>
+      <div class="row">
+        <div class="col d-flex">
+          <div class="col-50 col-left">
+            <div class="logo-footer-container d-flex">
+              <img
+                class="footer-logo"
+                src="../assets/img/logo/bird-only.png"
+                alt="Insubria logo"
+              />
+              <div class="address">
+                <h4>Insubria</h4>
+                <span>ddf</span>
+              </div>
+              <div class="contacts">
+                <h4>fdf</h4>
+              </div>
+            </div>
           </div>
-          <div class="col-40">Ciao</div>
+          <div class="col-50 col-right">
+            <h2>CIAO</h2>
+          </div>
         </div>
       </div>
     </div>
@@ -33,11 +48,20 @@ footer {
   border-bottom: 1px solid rgba(255, 255, 255, 0.753);
 }
 
-.col-60 {
-  width: 60%;
+.col-50 {
+  width: 50%;
 }
 
-.col-40 {
-  width: 40%;
+.col-left {
+  background-color: red;
+
+  .logo-footer-container img {
+    width: 10%;
+    object-fit: contain;
+  }
+}
+
+.col-right {
+  background-color: blue;
 }
 </style>
