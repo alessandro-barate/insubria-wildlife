505e06b15337a0e5e193687d01c740717716f9fb
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-11-19 20:16:15 +0100

    Modifying the footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -9,53 +9,47 @@ export default {
     <div class="container">
       <div class="row">
         <div class="col d-flex">
-          <div class="col-70 col-left">
-            <div class="logo-footer-container d-flex">
+          <div class="logo-footer-container">
+            <img
+              class="footer-logo"
+              src="../assets/img/logo/bird-only.png"
+              alt="Insubria logo"
+            />
+          </div>
+          <div class="address d-flex">
+            <div class="location-container d-flex">
               <img
-                class="footer-logo"
-                src="../assets/img/logo/bird-only.png"
-                alt="Insubria logo"
+                src="../assets/img/footer-img/location-icon.png"
+                alt="Logo GPS"
               />
-              <div class="address d-flex">
-                <div class="location-container d-flex">
-                  <img
-                    src="../assets/img/footer-img/location-icon.png"
-                    alt="GPS icon"
-                  />
-                </div>
-                <div class="address-container">
-                  <h3>Insubria</h3>
-                  <a href=""> Via Roma 20, 20004, Arluno </a>
-                </div>
-              </div>
-              <div class="contacts">
-                <h3>Contattaci</h3>
-                <div class="phone-container d-flex">
-                  <img
-                    src="../assets/img/footer-img/phone-icon.png"
-                    alt="Icona del telefono"
-                  />
-                  <a href="">0230256512</a>
-                </div>
-                <div class="mail-container d-flex">
-                  <img
-                    src="../assets/img/footer-img/mail-icon.png"
-                    alt="Immagine email"
-                  />
-                  <a href="">fewfew@fewf.com</a>
-                </div>
+            </div>
+            <div class="address-container">
+              <h3>Insubria</h3>
+              <a href=""> Via Roma 20, 20004, Arluno </a>
+              <div>
+                <h3>Codice Fiscale</h3>
+                <span>01234567890</span>
               </div>
             </div>
           </div>
-          <div class="col-30 col-right d-flex">
-            <!--<div class="col-50">
-              <div class="button-container">
-                <p>Hai trovato un esemplare di animale e non sai cosa fare?</p>
-                <button>Ti aiutiamo noi!</button>
-              </div>
-            </div> -->
-            <div class="">CIAO</div>
+          <div class="contacts">
+            <h3>Contattaci</h3>
+            <div class="phone-container d-flex">
+              <img
+                src="../assets/img/footer-img/phone-icon.png"
+                alt="Icona telefono"
+              />
+              <a href="">0230256512</a>
+            </div>
+            <div class="mail-container d-flex">
+              <img
+                src="../assets/img/footer-img/mail-icon.png"
+                alt="Immagine email"
+              />
+              <a href=""> fewfew@fewf.com </a>
+            </div>
           </div>
+          <div class="socials d-flex">4</div>
         </div>
       </div>
     </div>
@@ -88,30 +82,32 @@ a {
   border-bottom: 1px solid rgba(255, 255, 255, 0.753);
 }
 
-.col-30 {
-  width: 30%;
-}
-
-.col-50 {
-  width: 50%;
-}
-
-.col-70 {
-  width: 70%;
+.logo-footer-container,
+.address,
+.contacts,
+.socials {
+  width: 25%;
 }
 
 .logo-footer-container img {
-  width: 12%;
-  object-fit: contain;
+  width: 30%;
+  margin-left: 60px;
 }
 
-.address {
-  width: 30%;
+.address img {
+  width: 30px;
+  height: 30px;
+  margin: 0;
+}
 
-  img {
-    width: 30px;
-    margin-right: 10px;
-  }
+.address-container {
+  margin-left: 20px;
+}
+
+.location-container {
+  align-items: center;
+  width: 10%;
+  margin: 0;
 }
 
 .contacts {
@@ -163,7 +159,12 @@ a {
 // Media queries
 @media (max-width: 768px) {
   .logo-footer-container {
-    margin-left: -20px;
+    width: 0%;
+    margin: 0;
+  }
+
+  .location-container {
+    margin-right: 25px;
   }
 
   .footer-logo {
