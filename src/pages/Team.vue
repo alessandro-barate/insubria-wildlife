98fa3ad02479d7f6010643abb6402b557af4369e
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-11-21 20:01:34 +0100

    Styling the team members cards

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -24,14 +24,16 @@ export default {
               <div
                 v-for="(member, index) in store.members"
                 :key="index"
-                class="col-25"
+                class="col-33"
               >
                 <div class="member-img">
                   <img :src="member.image" alt="" />
                 </div>
                 <h2>{{ member.name }}</h2>
                 <h3>{{ member.title }}</h3>
-                <p>{{ member.description }}</p>
+                <div class="info-btn">
+                  <button>Scopri di più</button>
+                </div>
               </div>
             </div>
           </div>
@@ -48,28 +50,71 @@ export default {
   padding-bottom: 50px;
   text-align: center;
   background-image: url(/team/background/team-group.jpg);
-  background-size: cover;
+  background-size: 100%;
 }
 
 .overlay {
   width: 90%;
   background-color: rgba(0, 0, 0, 0.8);
+
+  h1 {
+    font-size: 45px;
+    padding-top: 30px;
+    padding-bottom: 60px;
+  }
 }
 
-.col-25 {
-  width: 25%;
+.col-33 {
+  padding-top: 30px;
+  padding-bottom: 30px;
+  margin-bottom: 50px;
+  border-radius: 30px;
+  background-color: rgba(0, 0, 0, 0.6);
+  width: calc(100% / 3 - 50px);
+
+  .info-btn {
+    width: 100%;
+    padding-top: 10px;
+
+    button {
+      border: none;
+      font-size: 15px;
+      font-weight: bold;
+      border-radius: 20px;
+      padding: 8px 25px 8px 25px;
+
+      &:hover {
+        color: white;
+        background-color: rgb(94, 94, 94);
+      }
+    }
+  }
 }
 
 .cards-container {
   flex-wrap: wrap;
+
+  h2 {
+    padding-bottom: 10px;
+  }
+
+  h3 {
+    padding-bottom: 7px;
+  }
+
+  p {
+    width: 80%;
+  }
 }
 
 .member-img {
   width: 100%;
+  padding-bottom: 15px;
 
   img {
-    width: 25vh;
-    height: 25vh;
+    width: 35%;
+    height: 35%;
+    border: 3px solid #ff6b3a;
     object-fit: cover;
     border-radius: 110px;
   }
--- src/store.js
+++ src/store.js
@@ -54,14 +54,14 @@ export const store = reactive({
     {
       name: "Giulia",
       title: "Consigliera",
-      image: "team/grid/giulia.jpg",
+      image: "team/grid/giulia.png",
       description:
         "Ricordo ancora nitidamente quando iniziò ad affacciarsi tra i miei pensieri la passione per gli animali. A 7 anni, però, non ne sei ancora pienamente consapevole. Un rapace urtò accidentalmente contro la porta di casa dei miei nonni; lo raccolsi con molta attenzione, scesi in giardino, e quando allentai leggermente la presa, si librò in volo in un attimo. Qualcosa scattò in me quel giorno: un sentimento, un amore verso gli animali selvatici, che però non era ancora pronto a sbocciare completamente. Lo fece durante il mio percorso universitario, insieme alla sicurezza che non mi avrebbe mai più lasciato. Oggi ho fatto della mia passione il mio lavoro. Amo passare il tempo all’aria aperta, in mezzo ai boschi, in compagnia dei miei cani o anche solo in silenzio con la mia macchina fotografica, aspettando che un martin pescatore si posi sul ramo di fronte a me.",
     },
     {
       name: "Maura",
       title: "Consigliera",
-      image: "team/grid/maura.jpg",
+      image: "team/grid/maura.png",
       description:
         "Se la prima parola che pronunci quando sei piccola è “cane”, vuol dire che da qualche parte il tuo percorso è già tracciato. Diventare veterinaria è stato un sogno che è realizzato, ma l'incontro con la fauna selvatica si è preso una grossa parte del mio cuore. Durante il progetto Erasmus in una clinica universitaria ebbi come paziente una piccola lontra, seguita poi da rapaci e pipistrelli: queste esperienze non fecero altro che rafforzare il mio amore per gli animali selvatici, culminato con un tirocinio presso un CRAS, dove ho scoperto il mondo della fauna locale e il valore del volontariato.",
     },
