526fd6414d9ad94a21e23c4de95709b35f60ae51
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2025-06-17 19:31:03 +0200

    Adding new event

--- src/store.js
+++ src/store.js
@@ -24,7 +24,8 @@ import speakerMarcoColombo from "./assets/img/events/speakers/marco-colombo.webp
 import event2025Aprile12 from "./assets/img/events/posters/2025/Aprile/12-04-25.webp";
 import event2025Maggio5 from "./assets/img/events/posters/2025/Maggio/30-05-25.webp";
 import event2025Aprile29 from "./assets/img/events/posters/2025/Aprile/29-04-25.webp";
-import event2025Giugno28 from "./assets/img/events/posters/2025/Giugno/28-06-25.jpg";
+import event2025Giugno22 from "./assets/img/events/posters/2025/Giugno/22-06-25.webp";
+import event2025Giugno28 from "./assets/img/events/posters/2025/Giugno/28-06-25.webp";
 
 // Importing the flow chart images
 import italianFlowChart from "./assets/img/sos-animal/flow-chart.webp";
@@ -560,7 +561,7 @@ export const store = reactive({
       }),
     },
 
-    // CurioCity
+    // BioBlitz
     {
       title: computed(() => {
         const { t } = useI18n();
@@ -590,10 +591,49 @@ export const store = reactive({
         const { t } = useI18n();
         return t("events.5.ticket");
       }),
-      poster: event2025Giugno28,
+      poster: event2025Giugno22,
       description: computed(() => {
         const { t } = useI18n();
         return t("events.5.description");
+      }),
+
+      speakers: computed(() => []),
+    },
+
+    // CurioCity
+    {
+      title: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.title");
+      }),
+      date: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.date");
+      }),
+      alt: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.alt");
+      }),
+      location: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.location");
+      }),
+      price: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.price");
+      }),
+      calendar: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.calendar");
+      }),
+      ticket: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.ticket");
+      }),
+      poster: event2025Giugno28,
+      description: computed(() => {
+        const { t } = useI18n();
+        return t("events.6.description");
       }),
 
       speakers: computed(() => []),
